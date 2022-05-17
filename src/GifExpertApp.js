436e5afb6ebb377c7c32fe766475c56b58629b70
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2022-05-16 22:26:15 -0500

    se crea componente gifExpertApp

---
+++ src/GifExpertApp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const GifExpertApp = props => {
+  return (
+    <>
+      <h2>Gif expert app</h2>
+      <hr />  
+    </>
+  )
+}
+
+GifExpertApp.propTypes = {
+
+}
